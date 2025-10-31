9dc8754ed1a14308fb00c35f2f57c1e7345faa54
Seyda nur <seydanuraydin50@gmail.com>
2025-10-31 17:18:08 +0300

    Fix formatting and improve morphological feature output

--- Morphological_featuresof Word.py
+++ Morphological_featuresof Word.py
@@ -21,4 +21,48 @@ for token in doc:
     print(f"Is alpha: {token.is_alpha}")  # is the token alphabetic?
     print(f"Is stop: {token.is_stop}")  # is the token a stop word?
     print(f"Morphology: {token.morph}")  # morphological features
+
     print(f"Is plural: {'Number=Plur' in token.morph}")
+
+
+    
+""" text: I
+    lemma: I
+    POS: PRON
+    Tag: PRP
+    Dependency: nsubj
+    Shape: X
+    Is alpha: True
+    Is stop: True
+    Morphology: Case=Nom|Number=Sing|Person=1|PronType=Prs
+    Is plural: False
+    text: go
+    lemma: go
+    POS: VERB
+    Tag: VBP
+    Dependency: ROOT
+    Shape: xx
+    Is alpha: True
+    Is stop: True
+    Morphology: Tense=Pres|VerbForm=Fin
+    Is plural: False
+    text: to
+    lemma: to
+    POS: ADP
+    Tag: IN
+    Dependency: prep
+    Shape: xx
+    Is alpha: True
+    Is stop: True
+    Morphology: 
+    Is plural: False
+    text: school
+    lemma: school
+    POS: NOUN
+    Tag: NN
+    Dependency: pobj
+    Shape: xxxx
+    Is alpha: True
+    Is stop: False
+    Morphology: Number=Sing
+    Is plural: False"""
